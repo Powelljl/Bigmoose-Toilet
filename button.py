a5b576bd99b08173fc4bb3b9ab2919e9dedeeb0e
Isabella Szemeti <issythegirl@gmail.com>
2020-09-04 01:13:33 +0100

    chore(button.py): Refactored code

--- button.py
+++ button.py
@@ -1,31 +1,26 @@
 import RPi.GPIO as GPIO
 import time
-import threading
 from pygame import mixer
 
 mixer.init()
 GPIO.setmode(GPIO.BCM)
-INPUT = 24
+INPUT_PIN = 24
 GPIO.setwarnings(False)
-GPIO.setup([INPUT], GPIO.IN , pull_up_down=GPIO.PUD_DOWN)
-isFirstPress = 1
-def music_thread():
-    mixer.music.load('/home/pi/Bigmoose/Bigmoose-Toilet/audio/Moose-Sound.mp3')
-    mixer.music.play ()
+GPIO.setup(INPUT_PIN, GPIO.IN , pull_up_down=GPIO.PUD_DOWN)
+isFirstPress = True
+
+def music():
+    mixer.music.load('./audio/Moose-Sound.mp3')
+    mixer.music.play()
 
 def handle(pin):
     global isFirstPress
-    if isFirstPress == 1:
-      t = threading.Thread(target=music_thread)
-      t.daemon = True
-      t.start()
-      print("starting playback")
-      isFirstPress = 0
-      time.sleep(3)  
+    if isFirstPress == True:
+        print("starting playback")
+        music()
+        isFirstPress = False
+        time.sleep(3)  
     else:
-        isFirstPress = 1
+        return
 
-GPIO.add_event_detect(INPUT, GPIO.RISING, handle)
-
-while True:
-    time.sleep(1e6)
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=handle)
